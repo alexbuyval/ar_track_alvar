d4bcf35d858c6609bde4436ec074200c56f7e7ab
alexbuyval <alexbuyval@gmail.com>
2016-01-03 13:07:49 +0300

    The capture by opencv was added

---
+++ nodes/IndividualMarkersNoKinectOpenCV.cpp
@@ -0,0 +1,196 @@
+/*
+ Software License Agreement (BSD License)
+
+ Copyright (c) 2012, Scott Niekum
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+  * Neither the name of the Willow Garage nor the names of its
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+
+ author: Scott Niekum
+*/
+
+
+#include "ar_track_alvar/CvTestbed.h"
+#include "ar_track_alvar/MarkerDetector.h"
+#include "ar_track_alvar/Shared.h"
+#include <cv_bridge/cv_bridge.h>
+#include <ar_track_alvar_msgs/AlvarMarker.h>
+#include <ar_track_alvar_msgs/AlvarMarkers.h>
+#include <sensor_msgs/image_encodings.h>
+#include <dynamic_reconfigure/server.h>
+#include <ar_track_alvar/ParamsConfig.h>
+
+using namespace alvar;
+using namespace std;
+
+bool init=true;
+Camera *cam;
+ros::Publisher arMarkerPub_;
+ar_track_alvar_msgs::AlvarMarkers arPoseMarkers_;
+MarkerDetector<MarkerData> marker_detector;
+
+bool enableSwitched = false;
+bool enabled = true;
+double max_frequency;
+double marker_size;
+double max_new_marker_error;
+double max_track_error;
+std::string cam_image_topic; 
+std::string cam_info_topic; 
+std::string output_frame;
+
+
+void configCallback(ar_track_alvar::ParamsConfig &config, uint32_t level)
+{
+  ROS_INFO("AR tracker reconfigured: %s %.2f %.2f %.2f %.2f", config.enabled ? "ENABLED" : "DISABLED",
+           config.max_frequency, config.marker_size, config.max_new_marker_error, config.max_track_error);
+
+  enableSwitched = enabled != config.enabled;
+
+  enabled = config.enabled;
+  max_frequency = config.max_frequency;
+  marker_size = config.marker_size;
+  max_new_marker_error = config.max_new_marker_error;
+  max_track_error = config.max_track_error;
+}
+
+int main(int argc, char *argv[])
+{
+	ros::init (argc, argv, "marker_detect");
+	ros::NodeHandle n, pn("~");
+	
+	if(argc < 5){
+		std::cout << std::endl;
+		cout << "Not enough arguments provided." << endl;
+		cout << "Usage: ./individualMarkersNoKinect <marker size in cm> <max new marker error> "
+		     << "<max track error> <cam image topic> <cam info topic> <output frame> [ <max frequency> ]";
+		std::cout << std::endl;
+		return 0;
+	}
+
+	// Get params from command line
+	marker_size = atof(argv[1]);
+	max_new_marker_error = atof(argv[2]);
+	max_track_error = atof(argv[3]);
+        output_frame = argv[4];
+	marker_detector.SetMarkerSize(marker_size);
+
+  if (argc > 5)
+    max_frequency = atof(argv[5]);
+
+  // Set dynamically configurable parameters so they don't get replaced by default values
+  pn.setParam("marker_size", marker_size);
+  pn.setParam("max_new_marker_error", max_new_marker_error);
+  pn.setParam("max_track_error", max_track_error);
+
+  if (argc > 5)
+    pn.setParam("max_frequency", max_frequency);
+
+	cam = new Camera();
+	arMarkerPub_ = n.advertise < ar_track_alvar_msgs::AlvarMarkers > ("ar_pose_marker", 0);
+	
+  // Prepare dynamic reconfiguration
+  dynamic_reconfigure::Server < ar_track_alvar::ParamsConfig > server;
+  dynamic_reconfigure::Server<ar_track_alvar::ParamsConfig>::CallbackType f;
+
+  f = boost::bind(&configCallback, _1, _2);
+  server.setCallback(f);
+
+    IplImage* ipl_image=0;
+  
+          CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY+1); //cvCaptureFromCAM( 0 );
+        assert( capture );
+
+        //cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);//1280); 
+        //cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);//960); 
+
+        // узнаем ширину и высоту кадра
+        double width = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+        double height = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+        ROS_INFO("Camera width %.0f height %.0f\n", width, height );
+	
+	//Give tf a chance to catch up before the camera callback starts asking for transforms
+  // It will also reconfigure parameters for the first time, setting the default values
+	ros::Duration(1.0).sleep();
+	ros::spinOnce();	
+	 
+
+  // Run at the configured rate, discarding pointcloud msgs if necessary
+  ros::Rate rate(max_frequency);
+  
+
+
+  
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    rate.sleep();
+
+    if (std::abs((rate.expectedCycleTime() - ros::Duration(1.0 / max_frequency)).toSec()) > 0.001)
+    {
+      // Change rate dynamically; if must be above 0, as 0 will provoke a segfault on next spinOnce
+      ROS_DEBUG("Changing frequency from %.2f to %.2f", 1.0 / rate.expectedCycleTime().toSec(), max_frequency);
+      rate = ros::Rate(max_frequency);
+    }
+
+			//Get the estimated pose of the main markers by using all the markers in each bundle
+			
+			ipl_image = cvQueryFrame( capture );
+			
+			marker_detector.Detect(ipl_image, cam, true, false, max_new_marker_error, max_track_error, CVSEQ, true);
+
+			arPoseMarkers_.markers.clear ();
+			for (size_t i=0; i<marker_detector.markers->size(); i++) 
+			{
+				//Get the pose relative to the camera
+				int id = (*(marker_detector.markers))[i].GetId(); 
+				Pose p = (*(marker_detector.markers))[i].pose;
+				//Create the pose marker messages
+				ar_track_alvar_msgs::AlvarMarker ar_pose_marker;
+				
+				ar_pose_marker.pose.pose.position.x = p.translation[0]/100.0;
+				ar_pose_marker.pose.pose.position.y = p.translation[1]/100.0;
+				ar_pose_marker.pose.pose.position.z = p.translation[2]/100.0;
+				ar_pose_marker.pose.pose.orientation.x = p.quaternion[0];
+				ar_pose_marker.pose.pose.orientation.y = p.quaternion[1];
+				ar_pose_marker.pose.pose.orientation.z = p.quaternion[2];
+				ar_pose_marker.pose.pose.orientation.w = p.quaternion[3];
+
+				ar_pose_marker.header.frame_id = output_frame;
+				ar_pose_marker.header.stamp = ros::Time::now();
+				ar_pose_marker.id = id;
+				arPoseMarkers_.markers.push_back (ar_pose_marker);	
+
+			}
+			arMarkerPub_.publish (arPoseMarkers_);
+
+  }
+
+    return 0;
+}
